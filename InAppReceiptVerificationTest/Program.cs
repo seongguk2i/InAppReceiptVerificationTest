785095a0d2280c9423e918c818205d5a32f2bbe4
김성국 <live8681@naver.com>
2022-03-24 00:03:04 +0900

    테스트 ㅁ커밋

--- InAppReceiptVerificationTest/Program.cs
+++ InAppReceiptVerificationTest/Program.cs
@@ -7,8 +7,7 @@ namespace InAppReceiptVerificationTest
         static void Main(string[] args)
         {
             /*{"Payload":"{ \"this\" : \"is a fake receipt\" }","Store":"fake","TransactionID":"45940b8e-2516-435d-9ac4-c4dcd6cdecd7"}*/
-            var itemID = "heart";
-            CIOSReceiptVerificationMng.VerifyIOSReceipt(ref itemID, "{\"Payload\":\"{ \"this\" : \"is a fake receipt\" }\",\"Store\":\"fake\",\"TransactionID\":\"45940b8e-2516-435d-9ac4-c4dcd6cdecd7\"}", true);
+            CIOSReceiptVerificationMng.CheckReceipt("heart", "{\"Payload\":\"{ \"this\" : \"is a fake receipt\" }\",\"Store\":\"fake\",\"TransactionID\":\"45940b8e-2516-435d-9ac4-c4dcd6cdecd7\"}");
         }
     }
 }
